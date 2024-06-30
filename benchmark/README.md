c089115604f14da373c58a6d02139b0db50af9aa
Yonghao Shu <123shuyonghao@gmail.com>
2024-06-30 15:11:29 -0700

    Benchmark

---
+++ benchmark/README.md
@@ -0,0 +1,7 @@
+# Notes
+## Benchmarking
+```
+>> go test -bench=.
+>> go test -bench=. -benchtime=5s
+>> go test -bench=. -benchtime=1000000x
+```
---
+++ benchmark/bench_test.go
@@ -0,0 +1,72 @@
+package benchmark
+
+import (
+	kvdb "kvdb-go"
+	"kvdb-go/utils"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var db *kvdb.DB
+
+func init() {
+    options := kvdb.DefaultOptions
+    dir, _ := os.MkdirTemp("", "kvdb-go-benchmark-")
+    options.DirPath = dir
+
+    var err error
+    db, err = kvdb.Open(options)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func BenchmarkPut(b *testing.B) {
+    b.ResetTimer()
+    b.ReportAllocs()
+
+    for i := 0; i < b.N; i++ {
+        err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+        assert.Nil(b, err)
+    }
+}
+
+func BenchmarkGet(b *testing.B) {
+    for i := 0; i < 10000; i++ {
+        err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+        assert.Nil(b, err)
+    }
+
+    rng := rand.New(rand.NewSource(time.Now().Unix()))
+    b.ResetTimer()
+    b.ReportAllocs()
+
+    for i := 0; i < b.N; i++ {
+        _, err := db.Get(utils.GetTestKey(rng.Int()))
+        if err != nil && err != kvdb.ErrKeyNotFound {
+            b.Fatal(err)
+        }
+    }
+}
+
+func BenchmarkDelete(b *testing.B) {
+    for i := 0; i < 10000; i++ {
+        err := db.Put(utils.GetTestKey(i), utils.GetTestValue(1024))
+        assert.Nil(b, err)
+    }
+
+    rng := rand.New(rand.NewSource(time.Now().Unix()))
+    b.ResetTimer()
+    b.ReportAllocs()
+
+    for i := 0; i < b.N; i++ {
+        err := db.Delete(utils.GetTestKey(rng.Int()))
+        if err != nil && err != kvdb.ErrKeyNotFound {
+            b.Fatal(err)
+        }
+    }
+}
